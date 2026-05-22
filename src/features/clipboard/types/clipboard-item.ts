96cc2ac5a44bfd477838d24e18a02a7cb4849b1f
Sokhuong Uon <sokhuong.usk@gmail.com>
2026-05-22 15:25:57 +0700

    wip: prepare new clipboard item type

---
+++ src/features/clipboard/types/clipboard-item.ts
@@ -0,0 +1,42 @@
+import { Mime } from "@/features/clipboard/types/mime"
+
+export type ClipboardItem = {
+  id: number
+
+  sort_order: string
+
+  content?: string
+
+  /**
+   * The width of the image if it is one.
+   */
+  width?: number
+
+  /**
+   * The height of the image if it is one.
+   */
+  height?: number
+
+  hash: string
+  mime: Mime | string & {}
+
+  source_app?: string
+
+  is_favorite: boolean
+  note?: string
+
+  detected_date?: string
+
+  is_color: boolean
+
+  /**
+   * When `kv` is `_KVS`, the content is a set of key-value pairs.
+   * Otherwise, `kv` is the key of the key-value pair.
+   */
+  kv?: '_KVS' | string & {}
+
+  is_secret: boolean
+
+  created_at: string
+  updated_at: string
+}
---
+++ src/features/clipboard/types/mime.ts
@@ -0,0 +1,15 @@
+export type Mime =
+  | 'text/plain'
+
+  | 'text/html'
+  | 'text/rtf'
+  | 'application/json'
+
+  | 'image/png'
+  | 'image/jpeg'
+  | 'image/gif'
+  | 'image/webp'
+
+  | 'image/svg+xml'
+
+  | 'text/uri-list';
