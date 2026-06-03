9358c19bbe10202aa0bf6abe283698182bcd1b12
Sokhuong Uon <sokhuong.usk@gmail.com>
2026-06-03 08:23:47 +0700

    feat: can insert image

--- src-tauri/src/database/items/insert_image.rs
+++ src-tauri/src/database/items/insert_image.rs
@@ -1,13 +1,37 @@
-use drizzle::sqlite::prelude::*;
-
-use crate::crypto::hash_content::hash_content;
-use crate::detection::{color, date, env, file, secret};
-use crate::schema::*;
-
-use super::super::schema::*;
 use super::super::utils::*;
 use super::super::Database;
+use crate::database::items::get_new_max_sort_order::get_new_max_sort_order;
+use crate::schema::InsertClipboards;
+use chrono::Utc;
+
+use super::super::structs::insert_clipboard_db_params::InsertClipboardDbParams;
 
 impl Database {
-    pub fn insert_image(&self, params: InsertClipboardItemParams) {}
+    pub fn insert_image(&self, params: InsertClipboardDbParams) -> Result<(), String> {
+        let drizzle = self.lock()?;
+        let clipboards = &drizzle.schema.clipboards;
+
+        if let Some(image) = params.image {
+            let sort_order = get_new_max_sort_order(&drizzle)?;
+            let now = Utc::now().to_rfc3339();
+
+            drizzle
+                .db
+                .insert(*clipboards)
+                .values([InsertClipboards::new(
+                    sort_order,
+                    params.hash,
+                    false,
+                    false,
+                    false,
+                    now.clone(),
+                    now,
+                )
+                .with_image(image)])
+                .execute()
+                .map_err(error_to_string)?;
+        }
+
+        Ok(())
+    }
 }
