43e674244cd9d0a368a9045c35653e814d0cb1ad
Sokhuong Uon <sokhuong.usk@gmail.com>
2026-06-04 10:15:18 +0700

    chore: add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+          target: 'react',
+          autoCodeSplitting: true,
+        }),
+    , react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@": new URL("./src", import.meta.url).pathname,
