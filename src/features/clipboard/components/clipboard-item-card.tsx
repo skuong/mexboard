fb41bcc40c4609b7cb1ec0e22b1abcc18ecde69a
Sokhuong <sokhuong.usk@gmail.com>
2026-06-07 15:07:54 +0700

    chore: reduce ring opacity for clipboard item card

--- src/features/clipboard/components/clipboard-item-card.tsx
+++ src/features/clipboard/components/clipboard-item-card.tsx
@@ -6,7 +6,7 @@ export const ClipboardItemCard = memo(function ClipboardItem() {
 	const cardRef = useRef<HTMLDivElement>(null);
 
 	return (
-		<Card ref={cardRef} className="group py-2">
+		<Card ref={cardRef} className="group py-2 ring-accent/80">
 			<CardContent className="relative px-2">
 				<ClipboardItemContent />
 			</CardContent>
