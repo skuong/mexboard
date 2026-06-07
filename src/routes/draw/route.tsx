0b340429c829861c844b787d0e3bbdd48e657376
Sokhuong <sokhuong.usk@gmail.com>
2026-06-07 22:10:52 +0700

    feat: add excalidraw

--- src/routes/draw/route.tsx
+++ src/routes/draw/route.tsx
@@ -1,9 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Excalidraw } from '@excalidraw/excalidraw';
+import '@excalidraw/excalidraw/index.css';
+import { createFileRoute } from '@tanstack/react-router';
+import { useTheme } from 'next-themes';
 
 export const Route = createFileRoute('/draw')({
-  component: RouteComponent,
-})
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/draw"!</div>
+	const { resolvedTheme } = useTheme();
+
+	return (
+		<div className="h-screen w-screen overflow-hidden">
+			<Excalidraw theme={resolvedTheme === 'dark' ? 'dark' : 'light'} />
+		</div>
+	);
 }
