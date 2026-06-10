31f096156dac34b8809aa0a08a656f27c0fd7afc
Sokhuong <sokhuong.usk@gmail.com>
2026-06-10 12:02:01 +0700

    feat: can copy image to system clipboard on click

--- src-tauri/src/clipboard/commands/write_clipboard_item_to_system_clipboard.rs
+++ src-tauri/src/clipboard/commands/write_clipboard_item_to_system_clipboard.rs
@@ -17,12 +17,21 @@ pub async fn write_clipboard_item_to_system_clipboard(
 
             if let Some(content) = clipboard.content {
                 let _ = mexboard.write_text(content).await;
-            } else if let Some(image) = clipboard.image {
-                let width = clipboard.width.unwrap();
-                let height = clipboard.width.unwrap();
-                log::info!("width: {}", width);
-                log::info!("height: {}", height);
-                let _ = mexboard.write_image(image, width, height).await;
+            } else if let Some(image_in_webp) = clipboard.image {
+                match image::load_from_memory_with_format(&image_in_webp, image::ImageFormat::WebP)
+                {
+                    Ok(dynamic_image) => {
+                        let width = clipboard.width.unwrap();
+                        let height = clipboard.height.unwrap();
+
+                        let rgba_image = dynamic_image.to_rgba8();
+                        let rgba_bytes = rgba_image.into_raw();
+                        let _ = mexboard.write_image(rgba_bytes, width, height).await;
+                    }
+                    Err(err) => {
+                        log::error!("Failed to decode image from database: {err}");
+                    }
+                }
             }
         }
         Err(_) => {
