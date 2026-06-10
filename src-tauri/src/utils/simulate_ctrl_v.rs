de2878f4edbb3be27c6bc6d4706451a9d56074a8
Sokhuong <sokhuong.usk@gmail.com>
2026-06-10 17:00:14 +0700

    fix: make pasting onto other app more reliable by adding some delay

--- src-tauri/src/utils/simulate_ctrl_v.rs
+++ src-tauri/src/utils/simulate_ctrl_v.rs
@@ -1,15 +1,30 @@
 use enigo::{Direction, Enigo, Key, Keyboard, Settings};
+use std::time::Duration;
+use tokio::time::sleep;
 
-pub fn simulate_ctrl_v() -> Result<(), String> {
+pub async fn simulate_ctrl_v() -> Result<(), String> {
     let mut enigo = Enigo::new(&Settings::default()).map_err(|err| err.to_string())?;
+
+    #[cfg(target_os = "macos")]
+    let modifier = Key::Command;
+    #[cfg(not(target_os = "macos"))]
+    let modifier = Key::Control;
+
+    sleep(Duration::from_millis(150)).await;
+
     enigo
-        .key(Key::Control, Direction::Press)
+        .key(modifier, Direction::Press)
         .map_err(|err| err.to_string())?;
+    sleep(Duration::from_millis(20)).await;
+
     enigo
         .key(Key::Unicode('v'), Direction::Click)
         .map_err(|err| err.to_string())?;
+    sleep(Duration::from_millis(20)).await;
+
     enigo
-        .key(Key::Control, Direction::Release)
+        .key(modifier, Direction::Release)
         .map_err(|err| err.to_string())?;
+
     Ok(())
 }
